6b2d862da38cc3852735767140ec8965f95d8d8f
LotuxPunk <vandendaelenclement@gmail.com>
2019-05-02 14:11:03 +0200

    dbConnection

---
+++ src/com/immanager/model/DataBaseConnection.java
@@ -0,0 +1,42 @@
+package com.immanager.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * @desc Singleton which allows you to connect to the database
+ */
+public final class DataBaseConnection {
+    private Connection connection;
+    private static DataBaseConnection instance;
+
+    private DataBaseConnection(){
+        String url = "jbdc:mysql://localhost:3306/";
+        String dbName = "immanager";
+        String driver = "com.mysql.jbdc.Driver";
+        String username = "root";
+        String password = "vdd2019";
+
+        try {
+            Class.forName(driver).newInstance();
+            this.connection = (Connection)DriverManager.getConnection(url+dbName,username,password);
+        }
+        catch (Exception sqlE){
+            sqlE.printStackTrace();
+        }
+    }
+
+    /**
+     * @return DataBaseConnection object
+     */
+    public static synchronized DataBaseConnection getInstance() {
+        if ( instance == null ) {
+            instance = new DataBaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
