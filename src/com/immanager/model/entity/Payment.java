d8cd49508d68703207994fd7137621771b2d3a11
LotuxPunk <vandendaelenclement@gmail.com>
2019-05-02 14:24:01 +0200

    entities reimplementation

---
+++ src/com/immanager/model/entity/Payment.java
@@ -0,0 +1,29 @@
+package com.immanager.model.entity;
+
+import java.util.GregorianCalendar;
+
+public class Payment {
+    private double amount;
+    private GregorianCalendar date;
+
+    public Payment (double amount, GregorianCalendar date) {
+        setAmount(amount);
+        setDate(date);
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public GregorianCalendar getDate() {
+        return date;
+    }
+
+    public void setDate(GregorianCalendar date) {
+        this.date = date;
+    }
+}
